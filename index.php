09ef6b0adac6fbffb60231d9b05062d8e4e3882c
JormanRH <122406364+JormanRH@users.noreply.github.com>
2023-10-10 15:39:55 -0600

    Creación main

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>SPARTA</title>
+        <meta name="description" content="GRID ">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="Img/logo.png"/>
+        <link rel="stylesheet" href="Css/maquestilo.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    </head>
+    <body>
+        <header class="header">
+            <?php
+                include './menuRespo.html';
+            ?>
+            <!--nav id="menuImg">
+                <a href="index.php">
+                    <img src="Img/logo.png" height="50px"/>
+                </a>
+
+            </nav>
+            <nav id="menu">
+                <a id="a" href="index.php"> Inicio</a>
+                <a id="a" href="AboutUs.php">Nosotros</a>
+                <a id="a" href="Contact.php">Productos</a> 
+                <a id="a" href="Register.php">Tiendas</a> 
+                <a id="a" href="Stores.php">Contacto</a> 
+            </nav-->
+        </header>
+        <nav class="nav">
+            BANNER
+        </nav>
+        <main class="main">
+            <div class="mision">
+                <h2>MISIÓN</h2>
+                <h3 id="justiH">Nuestra misión es brindar soluciones en el campo del deporte a nivel nacional e internacional a través de la innovación, servicio y  calidad de los productos que ofrecemos, nuestros recursos están destinados a contribuir con el desarrollo de la salud física y mental logrando así cumplir con el compromiso adquirido con nuestro público meta.</h3>
+            </div>
+        </main>
+        <section class="sidebar">
+            <div class="vision">
+                <h2>VISIÓN</h2>
+                <h3 id="justiH">Buscar posicionamiento América, brindando productos de alta calidad para la práctica del deporte profesional y/o recreativo, ofreciendo soluciones para las distintas necesidades,  utilizando una plataforma tecnológica que simplifique y facilite los procesos de las distintas áreas,  comprometidos a apoyar la salud mental y física de nuestro público.</h3>
+            </div>
+        </section>
+        <section class="valores">
+            <div class="value">
+                <h2>VALORES</h2>
+                <ul id="justiUl">
+                    <li><b>Respeto:</b> Es reconocer, apreciar y valorar a mi persona, así como a los demás, y a mi entorno. Es establecer hasta dónde llegan mis posibilidades de hacer o no hacer, como base de toda convivencia en sociedad.</li>
+                    <li><b>Integridad:</b> Es la demostración constante de actitudes positivas, aspirando a la eficacia, a la calidad y a la perfección humana de manera transparente.</li>
+                    <li><b>Perseverancia:</b>  Es alcanzar lo propuesto y buscar soluciones a las dificultades que puedan surgir.</li>
+                    <li><b>Humildad:</b>  Es reconocer nuestras debilidades, cualidades, capacidades y aprovecharlas para obrar en bien de los demás.</li>
+                    <li><b>Honestidad:</b>  Es armonizar las palabras con los hechos, es tener identidad y coherencia para estar orgulloso de sí mismo para con la empresa.</li>
+                </ul>
+                
+            </div>
+        </section>
+        <footer class="footer">
+            <div>
+                <a href="#" class="bi bi-facebook"></a><!--fa fa-facebook--> 
+                <a href="#" class="bi bi-instagram"></a>
+                <a href="#" class="bi bi-linkedin"></a>
+                <a href="#" class="bi bi-twitter"></a>
+                <a href="#" class="bi bi-whatsapp"></a>
+            </div>
+            <div>
+                <h3>Todos los Derechos Reservados 2023©</h3>
+            </div>
+        </footer>
+    </body>
+</html>
+
