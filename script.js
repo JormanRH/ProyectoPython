a053cb716b81e876a2c5dfbad7432eda8979f6c4
JormanRH <122406364+JormanRH@users.noreply.github.com>
2023-10-15 11:31:38 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const iconoMenu = document.querySelector('.icono-menu');
-    const menu = document.querySelector('.menu');
-
-    iconoMenu.addEventListener('click', function () {
-        menu.classList.toggle('active');
-    });
-});
