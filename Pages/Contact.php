ff5d121ba3e21fe2217224c7c82f03990ed6e03a
JormanRH <122406364+JormanRH@users.noreply.github.com>
2023-10-19 17:32:03 -0600

    Update Contact.php

--- Pages/Contact.php
+++ Pages/Contact.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="../Css/contactPage.css">
         <link rel="stylesheet" href="../Css/bootstrap.min.css" >
         <script src="../Process/JS/sendEmail.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
